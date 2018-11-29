f1d879d349063a158727fed66cd0f9fa2e754929
itacirgabral <itacirgabral@users.noreply.github.com>
2018-11-29 22:34:13 +0000

    task 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
 import Contact from './ContactComponent'
+import About from './AboutComponent'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends React.Component {
@@ -46,6 +47,7 @@ class Main extends React.Component {
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
         <Route path="/menu/:dishId" component={DishWithId} />
         <Route exact path="/contactus" component={Contact} />
+        <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
         <Redirect to="/home" />
       </Switch>
       <Footer />
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
 import { Link } from 'react-router-dom'
 
-function RenderMenuItem({ dish, onClick }) {
+function RenderMenuItem({ dish}) {
   return <Card >
     <Link to={`/menu/${dish.id}`}> 
       <CardImg width="100%" src={dish.image} alt={dish.name} />
