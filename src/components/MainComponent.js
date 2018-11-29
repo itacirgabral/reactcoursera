6f22633ed9b62e350f7f673a2e120342495a1ae4
itacirgabral <itacirgabral@users.noreply.github.com>
2018-11-29 19:16:01 +0000

    SPA1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,29 +3,43 @@ import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './MenuComponent'
 import DishDetail from './DishdetailComponent'
 import { DISHES } from '../shared/dishes'
+import { COMMENTS } from '../shared/comments'
+import { PROMOTIONS } from '../shared/promotions'
+import { LEADERS } from '../shared/leaders'
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import Home from './HomeComponent'
+import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends React.Component {
 
+
   constructor(props) {
     super(props);
+
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
-
-  
-
   render() {
+
+    const HomePage = () => <Home 
+          dish={this.state.dishes.find((dish) => dish.featured)}
+          promotion={this.state.promotions.find((promo) => promo.featured)}
+          leader={this.state.leaders.find((leader) => leader.featured)}
+        />
+
     return <>
       <Header />
       <Switch>
-        <Route path="/home" component={() => <Home />}/>
+        <Route path="/home" component={HomePage}/>
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+        <Route exact path="/contactus" component={Contact} />
         <Redirect to="/home" />
       </Switch>
       <Footer />
